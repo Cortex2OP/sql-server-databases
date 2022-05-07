3f6f08a4cc7c4429c067d140dc7e4e807cf83530
Sanjin-Pajic <pajicsanjo@gmail.com>
2022-05-08 00:46:43 +0200

    Final Practice 2

--- practice/Practice 2/Vjezbe2.sql
+++ practice/Practice 2/Vjezbe2.sql
@@ -15,7 +15,8 @@ GO
 -- Adresa, 40 karaktera
 -- Drzava, 2 karaktera fiksne dužine
 -- PostanskiBroj, 5 karaktera fiksne dužine
--- Ugovor, bit polje (obavezan unos)CREATE TABLE Prodaja.Autori 
+-- Ugovor, bit polje (obavezan unos)
+CREATE TABLE Prodaja.Autori 
 (
 AutorID VARCHAR(11) CONSTRAINT PK_Autor PRIMARY KEY,
 Prezime VARCHAR(40) NOT NULL,
@@ -60,5 +61,55 @@ ADD CONSTRAINT FK_Knjiga_Izdavac FOREIGN KEY(IzdavacID) REFERENCES Prodaja.Izdav
 -- AutorID 11 karaktera, spoljni ključ
 -- KnjigaID 6 karaktera, spoljni ključ
 -- AuOrd kratki cjelobrojni tip podatka
--- **Definisati primarni ključCREATE TABLE Prodaja.AutoriKnjige (AutorID VARCHAR(11) CONSTRAINT FK_AutoriKnjige_Autori FOREIGN KEY REFERENCES Prodaja.Autori(AutorID),KnjigaID VARCHAR(6) CONSTRAINT FK_AutoriKnjige_Knjige FOREIGN KEY REFERENCES Prodaja.Knjige(KnjigaID),AuOrd TINYINTCONSTRAINT PK_AutoriKnjige PRIMARY KEY(AutorID, KnjigaID))--8. U kreirane tabele izvršiti insert podataka iz baze Pubs (Za polje biljeska tabele Knjige na
---mjestima gdje je vrijednost NULL pohraniti „nepoznata vrijednost“)INSERT INTO Prodaja.AutoriSELECT A.au_id, A.au_lname, A.au_fname, A.phone, A.address, A.state, A.zip, A.contractFROM pubs.dbo.authors AS A-------------------------------------------INSERT INTO Prodaja.KnjigeSELECT t.title_id, t.title, t.type, t.pub_id, t.price, ISNULL(t.notes, 'NEPOZNATA VRIJEDNOST'), t.pubdateFROM pubs.dbo.titles AS t-------------------------------------------
+-- **Definisati primarni ključ
+CREATE TABLE Prodaja.AutoriKnjige 
+(
+AutorID VARCHAR(11) CONSTRAINT FK_AutoriKnjige_Autori FOREIGN KEY REFERENCES Prodaja.Autori(AutorID),
+KnjigaID VARCHAR(6) CONSTRAINT FK_AutoriKnjige_Knjige FOREIGN KEY REFERENCES Prodaja.Knjige(KnjigaID),
+AuOrd TINYINT
+CONSTRAINT PK_AutoriKnjige PRIMARY KEY(AutorID, KnjigaID)
+)
+--8. U kreirane tabele izvršiti insert podataka iz baze Pubs (Za polje biljeska tabele Knjige na
+--mjestima gdje je vrijednost NULL pohraniti „nepoznata vrijednost“)
+INSERT INTO Prodaja.Autori
+SELECT A.au_id, A.au_lname, A.au_fname, A.phone, A.address, A.state, A.zip, A.contract
+FROM pubs.dbo.authors AS A
+-------------------------------------------
+INSERT INTO Prodaja.Knjige
+SELECT t.title_id, t.title, t.type, t.pub_id, t.price, ISNULL(t.notes, 'NEPOZNATA VRIJEDNOST'), t.pubdate
+FROM pubs.dbo.titles AS t
+-------------------------------------------
+INSERT INTO Prodaja.AutoriKnjige
+SELECT A.au_id, A.title_id, A.au_ord
+FROM pubs.dbo.titleauthor AS A
+--9. U tabeli Autori nad kolonom Adresa promijeniti tip podatka na nvarchar (40)
+ALTER TABLE Prodaja.Autori
+ALTER COLUMN Adresa NVARCHAR(40)
+--10. Prikazati sve autore čije ime počinje sa slovom A ili S
+SELECT A.Ime
+FROM Prodaja.Autori AS A
+WHERE A.Ime LIKE 'A%' OR A.Ime LIKE 'S%'
+--11. Prikazati knjige gdje cijena nije unesena
+SELECT *
+FROM Prodaja.Knjige AS A
+WHERE A.Cijena IS NULL
+--12. U bazi Vjezba2 kreirati šemu Narudzbe
+GO
+CREATE SCHEMA Narudzbe
+GO
+--13. Upotrebom insert naredbe iz tabele Region baze Northwind izvršiti kreiranje i insertovanje
+--podataka u tabelu Regije šeme Narudžbe
+SELECT *
+INTO Narudzbe.Regije
+FROM Northwind.dbo.Region
+--14. Prikazati sve podatke koji se nalaze u tabeli Regije
+SELECT * 
+FROM Narudzbe.Regije
+--15. U tabelu Regije insertovati zapis:
+--   5 SEINSERT INTO Narudzbe.RegijeVALUES(5, 'SE')--16. U tabelu Regije insertovati zapise:
+--6 NE
+--7 NWINSERT INTO Narudzbe.RegijeVALUES(6, 'NE'), (7, 'NW')--17. Upotrebom insert naredbe iz tabele OrderDetails baze Northwind izvršiti kreiranje i
+--insertovanje podataka u tabelu StavkeNarudzbe šeme NarudzbeSELECT *INTO Narudzbe.StavkeNarudzbeFROM Northwind.dbo.[Order Details]--18. U tabeli StavkeNarudzbe dodati standardnu kolonu ukupno tipa decimalni broj (8,2).ALTER TABLE Narudzbe.StavkeNarudzbeADD Ukupno DECIMAL(8,2)UPDATE Narudzbe.StavkeNarudzbeSET Ukupno = Quantity*UnitPrice--20. U tabeli StavkeNarduzbe dodati izračunatu kolonu CijeliDio u kojoj će biti cijeli dio iz kolone UnitPriceALTER TABLE Narudzbe.StavkeNarudzbeADD CijeliDio AS FLOOR(UnitPrice)--21. U tabeli StavkeNarduzbe kreirati ograničenje na koloni Discount kojim će se onemogućiti unos
+--vrijednosti manjih od 0.
+ALTER TABLE Narudzbe.StavkeNarudzbe
+ADD CONSTRAINT CK_Discount CHECK(Discount>=0)
