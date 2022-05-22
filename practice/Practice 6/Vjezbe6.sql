ad8416a7e4aa663d5e5d4ac35326dd52cf38105b
Sanjin-Pajic <pajicsanjo@gmail.com>
2022-05-22 23:57:16 +0200

    initial

---
+++ practice/Practice 6/Vjezbe6.sql
@@ -0,0 +1,7 @@
+﻿--1. Prikazati tip popusta, naziv prodavnice i njen id. (Pubs)
+SELECT D.discounttype, S.stor_name, S.stor_id
+FROM dbo.stores AS S
+INNER JOIN dbo.discounts AS D
+ON D.stor_id = S.stor_id
+--2. Prikazati ime uposlenika, njegov id, te naziv posla koji obavlja. (Pubs)SELECT E.fname + ' ' + E.lname AS 'Ime i prezime', E.emp_idFROM dbo.employee AS EINNER JOIN jobs AS JON J.job_id = E.job_id--3. Prikazati spojeno ime i prezime uposlenika, teritoriju i regiju koju pokriva. Uslov je da
+--su zaposlenici mlađi od 60 godina. (Northwind)
